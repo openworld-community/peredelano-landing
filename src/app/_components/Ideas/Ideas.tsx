294015582d4439dd0d95c44e4a52d461bffe9f16
Ivan <30601836+wanku-x@users.noreply.github.com>
2023-06-18 19:36:31 +0400

    Added Ideas block (#9)

---
+++ src/app/_components/Ideas/Ideas.tsx
@@ -0,0 +1,26 @@
+import { Button } from '@/components'
+
+import styles from './Ideas.module.css'
+
+export default function Ideas() {
+	return (
+		<section className={styles.ideas}>
+			<div className={styles.ideas__container}>
+				<h2 className={styles.ideas__title}>Есть идея, которая может изменить мир?</h2>
+				<p className={styles.ideas__description}>
+					Если у&nbsp;тебя есть идеи проектов&nbsp;&mdash; смело пиши, что хочешь сделать,
+					кто и&nbsp;что тебе для этого нужно
+				</p>
+				<Button
+					variant="contained"
+					color="secondary"
+					as="a"
+					target="_blank"
+					href="https://t.me/peredelanoconfjunior/12272"
+				>
+					Чат в&nbsp;Telegram
+				</Button>
+			</div>
+		</section>
+	)
+}
