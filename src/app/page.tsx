86784f621abd904912eb2dd677cc8cd92dcc7be7
wanku-x <wanku@bk.ru>
2023-06-18 19:34:53 +0400

    Added Ideas block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-import { Link } from '@/components'
+import { Ideas } from './_components'
 
 export default function Home() {
-	return (
-		<>
-			<h1>Hello world</h1>
-			<p style={{ color: '#fff' }}>
-				Lorem ipsum dolor sit,{' '}
-				<Link as="a" href="#d">
-					amet
-				</Link>{' '}
-				consectetur adipisicing elit. Fugit sit possimus tempora? Quidem , facere maiores,
-				provident debitis delectus optio voluptatibus laboriosam neque expedita voluptates
-				incidunt repellat, quae molestiae assumenda?
-			</p>
-		</>
-	)
+	return <Ideas />
 }
