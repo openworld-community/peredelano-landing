4d1ee5df1d8589de47a307121b20ebfcbc686b33
Ivan <30601836+wanku-x@users.noreply.github.com>
2023-06-19 19:53:46 +0400

    Added Footer (#19)

---
+++ src/layouts/Footer/Footer.tsx
@@ -0,0 +1,80 @@
+import { cx } from 'class-variance-authority'
+import Link from 'next/link'
+
+import { contactsLinks, navigationLinks, teamLinks } from '@/constants'
+
+import styles from './Footer.module.css'
+
+export default function Vacancies() {
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.footer__container}>
+				<div className={styles.footer__grid}>
+					<div>
+						<img
+							className={styles.footer__logo}
+							src="/images/logo.svg"
+							alt="Peredelano Startups"
+							loading="lazy"
+						/>
+					</div>
+					<div>
+						<h2 className={styles.footer__title}>Навигация</h2>
+						<ul className={styles.list}>
+							{navigationLinks.map(({ id, name, href }) => (
+								<li className={styles.list__item} key={id}>
+									<Link className={styles.list__link} href={href}>
+										{name}
+									</Link>
+								</li>
+							))}
+						</ul>
+					</div>
+					<div>
+						<h2 className={styles.footer__title}>Контакты</h2>
+						<ul className={styles.list}>
+							{contactsLinks.map(({ id, description, name, href }) => (
+								<li className={styles.list__item} key={id}>
+									{description}
+									<br />
+									<Link
+										className={cx(
+											styles.list__link,
+											styles['list__link--underlined']
+										)}
+										href={href}
+									>
+										{name}
+									</Link>
+								</li>
+							))}
+						</ul>
+					</div>
+					<div>
+						<h2 className={styles.footer__title}>Над сайтом работали</h2>
+						<ul className={styles.list}>
+							{teamLinks.map(({ id, description, name, href }) => (
+								<li className={styles.list__item} key={id}>
+									{description}
+									<br />
+									<Link
+										className={cx(
+											styles.list__link,
+											styles['list__link--underlined']
+										)}
+										href={href}
+									>
+										{name}
+									</Link>
+								</li>
+							))}
+						</ul>
+					</div>
+				</div>
+				<small className={styles.footer__copyright}>
+					&copy;&nbsp;Peredelano Startups 2023
+				</small>
+			</div>
+		</footer>
+	)
+}
