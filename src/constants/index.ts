4d1e72f0a484254d48fa490ec21f61fce35e4a1e
Egor Samoylov <9168441394@bk.ru>
2023-06-20 22:43:17 +0300

    [LND-15] header (#18)
    * header
    
    * css fix

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,2 @@
 export * from './footerLinks'
+export { default as headerNavigationLinks } from './headerLinks'
--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,2 +1,3 @@
 /* eslint-disable import/prefer-default-export */
 export { default as Footer } from './Footer'
+export { default as Header } from './Header'
