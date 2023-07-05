b01a39cb25653ea16382b21def87f9e06296042a
S-E-A-JS <9168441394@bk.ru>
2023-07-05 22:30:09 +0600

    fix: GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import '@/styles/global.css'
 
 import { cx } from 'class-variance-authority'
 import { JetBrains_Mono } from 'next/font/google'
+import Script from 'next/script'
 
 import { Footer, Header } from '@/layouts'
 
@@ -29,20 +30,20 @@ export const metadata = {
 	},
 }
 
-const googleAnalitics = `<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-WP0LKMNBD2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
+export default function RootLayout({ children = null }: PropsWithChildren<{}>) {
+	return (
+		<html lang="ru" className={cx([jetBrainsMono.className, jetBrainsMono.variable])}>
+			{/* <!-- Google tag (gtag.js) --> */}
+			<Script async src="https://www.googletagmanager.com/gtag/js?id=G-WP0LKMNBD2" />
+			<Script id="google-analytics">
+				{`
+	window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-WP0LKMNBD2');
-</script>`
-
-export default function RootLayout({ children = null }: PropsWithChildren<{}>) {
-	return (
-		<html lang="ru" className={cx([jetBrainsMono.className, jetBrainsMono.variable])}>
-			<head>${googleAnalitics}</head>
+  `}
+			</Script>
 			<body className={styles.body}>
 				<Header />
 				{children}
